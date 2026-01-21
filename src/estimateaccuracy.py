137f7eb3e7194e1ed212039c7852dfbfbb216d24
Harshid <harshid.kkunka@gmail.com>
2026-01-21 17:03:07 -0600

    make benchmark

--- src/estimateaccuracy.py
+++ src/estimateaccuracy.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import readmap as map
 import random
+from tqdm import tqdm
 
 x_bnds = (0, 3.685794)
 y_bnds = (0, 8.069326)
@@ -11,16 +12,24 @@ launch_height = 0.5
 pos = map.getNumpyArrayPositions()
 states = map.getOptimalStates()
 
-random_pos = np.array([random.uniform(0, 3.685794), random.uniform(0, 8.069326)])
+total_dist = 0
+samples = 100
+for i in tqdm(range(samples)):
+    random_pos = np.array([random.uniform(0, 3.685794), random.uniform(0, 8.069326)])
 
-min = 100000
-index = 0
-for i in range(len(pos)):
-    dist = np.linalg.norm(pos[i] - random_pos)
-    if dist < min:
-        min = dist
-        index = i
+    min = 100000
+    index = 0
+    for i in range(len(pos)):
+        dist = np.linalg.norm(pos[i] - random_pos)
+        if dist < min:
+            min = dist
+            index = i
 
-poses, vel = sim.simulateShotDragLift(states[index][0][0], [random_pos[0], random_pos[1], sim.calculateRequiredHeading(sim.TARGET_POSE, [random_pos[0], random_pos[1], 0])], states[index][0][1], launch_height)
-sim.meshcatVisualizeShot(poses, sim.TARGET_POSE)
+
+    poses, vel = sim.simulateShotDragLift(states[index][0][0], [random_pos[0], random_pos[1], sim.calculateRequiredHeading(sim.TARGET_POSE, [random_pos[0], random_pos[1], 0])], states[index][0][1], launch_height)
+    dist, x, y = sim.findClosestPositionToTarget(poses, np.array(sim.TARGET_POSE))
+    total_dist += dist
+
+print("Average distance to target over 30 random shots: ", total_dist / samples)
+# sim.meshcatVisualizeShot(poses, sim.TARGET_POSE)
 # sim.graphSimulatedShot(poses, sim.TARGET_POSE)
